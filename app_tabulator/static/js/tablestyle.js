9aeb3f24a2bfa12f76ed49c93f5fe2d9f49ce6c2
foeGL <steffen_vogelmann@outlook.de>
2021-10-29 21:55:09 +0200

    checkbox läuft

--- app_tabulator/static/js/tablestyle.js
+++ app_tabulator/static/js/tablestyle.js
@@ -229,7 +229,6 @@ function formatCellValue(row, td, field){
             } else {      
                 var [fill, d] = getCheckboxFalse();  
             }        
-            console.log("fill: "+fill, "d: "+d)
             newPath.setAttributeNS(null, 'fill', fill)   
             newPath.setAttributeNS(null, "d", d)
                 
@@ -502,14 +501,13 @@ $('body').on('click', '.svgCheck', function(){
 });
 
 $('body').on('click', '.finishedCheckBox', function(){
-    var parent = $(this).parent();
     var checked = this.checked;
-    console.log(checked)
+    $(this).attr('value', checked);
 });
 
 
 document.addEventListener('mouseup', function(e) {
-    var box = document.getElementsByClassName('svgCheck-edit');
+    var box = document.getElementsByClassName('svgCheck-edit')[0];
     var childSVG = $(box).children('svg');
     var childBox = $(box).children('input');
     $(childSVG).css('display','inline')
@@ -517,22 +515,32 @@ document.addEventListener('mouseup', function(e) {
 
     var appearance = $(childSVG).attr('appearance')
     var childBoxValue = $(childBox).attr('value');
-    console.log(appearance)
-    console.log(childBoxValue)
-    if (appearance != childBoxValue){
-
+    if (appearance != childBoxValue){   
+        
+        var childPath = $(childSVG).children('path');   
+        console.log(childPath, $(childPath).attr('fill'))  
+        if (childBoxValue == 'true'){         
+            var [color, figure] = setCheckboxTrue();
+        } else {      
+            var [color, figure] = getCheckboxFalse();  
+        }        
+        console.log("set to :"+color+" and "+figure)
+        $(childPath).attr('fill', color);
+        $(childPath).attr('d', figure);
+        $(childSVG).attr('appearance',childBoxValue);
     }
+    $(box).removeClass('svgCheck-edit');
 });
 
 function setCheckboxTrue(){
     var fill = "#2DC214";
-    var d = "M21.652,3.211c-0.293-0.295-0.77-0.295-1.061,0L9.41,14.34  c-0.293,0.297-0.771,0.297-1.062,0L3.449,9.351C3.304,9.203,3.114,9.13,2.923,9.129C2.73,9.128,2.534,9.201,2.387,9.351  l-2.165,1.946C0.078,11.445,0,11.63,0,11.823c0,0.194,0.078,0.397,0.223,0.544l4.94,5.184c0.292,0.296,0.771,0.776,1.062,1.07  l2.124,2.141c0.292,0.293,0.769,0.293,1.062,0l14.366-14.34c0.293-0.294,0.293-0.777,0-1.071L21.652,3.211z";
+    var d = "M21.652,3.211c-0.293-0.295-0.77-0.295-1.061,0L9.41,14.34 c-0.293,0.297-0.771,0.297-1.062,0L3.449,9.351C3.304,9.203,3.114,9.13,2.923,9.129C2.73,9.128,2.534,9.201,2.387,9.351  l-2.165,1.946C0.078,11.445,0,11.63,0,11.823c0,0.194,0.078,0.397,0.223,0.544l4.94,5.184c0.292,0.296,0.771,0.776,1.062,1.07  l2.124,2.141c0.292,0.293,0.769,0.293,1.062,0l14.366-14.34c0.293-0.294,0.293-0.777,0-1.071L21.652,3.211z";
     return [fill, d]
 }
 
 
 function getCheckboxFalse(){
     var fill = "#CE1515";
-    var d = "M22.245,4.015c0.313,0.313,0.313,0.826,0,1.139l-6.276,6.27c-0.313,0.312-0.313,0.826,0,1.14l6.273,6.272  c0.313,0.313,0.313,0.826,0,1.14l-2.285,2.277c-0.314,0.312-0.828,0.312-1.142,0l-6.271-6.271c-0.313-0.313-0.828-0.313-1.141,0  l-6.276,6.267c-0.313,0.313-0.828,0.313-1.141,0l-2.282-2.28c-0.313-0.313-0.313-0.826,0-1.14l6.278-6.269  c0.313-0.312,0.313-0.826,0-1.14L1.709,5.147c-0.314-0.313-0.314-0.827,0-1.14l2.284-2.278C4.308,1.417,4.821,1.417,5.135,1.73  L11.405,8c0.314,0.314,0.828,0.314,1.141,0.001l6.276-6.267c0.312-0.312,0.826-0.312,1.141,0L22.245,4.015z";
+    var d = "M22.245,4.015c0.313,0.313,0.313,0.826,0,1.139l-6.276,6.27c-0.313,0.312-0.313,0.826,0,1.14l6.273,6.272 c0.313,0.313,0.313,0.826,0,1.14l-2.285,2.277c-0.314,0.312-0.828,0.312-1.142,0l-6.271-6.271c-0.313-0.313-0.828-0.313-1.141,0  l-6.276,6.267c-0.313,0.313-0.828,0.313-1.141,0l-2.282-2.28c-0.313-0.313-0.313-0.826,0-1.14l6.278-6.269  c0.313-0.312,0.313-0.826,0-1.14L1.709,5.147c-0.314-0.313-0.314-0.827,0-1.14l2.284-2.278C4.308,1.417,4.821,1.417,5.135,1.73  L11.405,8c0.314,0.314,0.828,0.314,1.141,0.001l6.276-6.267c0.312-0.312,0.826-0.312,1.141,0L22.245,4.015z";
     return [fill, d]
 }
