1e14eaeb42600408e3da54ded64810855d7f0ed6
foeGL <Steffen.Vogelmann@outlook.de>
2021-10-29 12:05:02 +0200

    Button

--- app_tabulator/static/js/tablestyle.js
+++ app_tabulator/static/js/tablestyle.js
@@ -56,8 +56,7 @@ function tableCreate() {
         var row = tableDataNested[key];
         insertRow(tbl, row)
         checkForChildren(tbl, row)
-    }
-    
+    }    
     body.appendChild(tbl);
 }
   
@@ -181,21 +180,7 @@ function handlePPB(tr, row){
         var td = tr.insertCell();
         td.classList.add('ppb-'+field)
         addTreeBranch(row, td, field)
-        var cellValue="";
-        switch(field){
-            case 'InvoiceType':
-                console.log("field: ", field)
-                console.log("row[field]: ", row[field])
-                if (row[field] < 1){
-                    cellValue = Invoice[row[field]]
-                } else {
-                    cellValue = 'Pauschale #'+(row[field])
-                }
-                break;
-            default:
-                cellValue = row[field]
-                break;
-        }
+        var cellValue = formatCellValue(row, field);
         td.appendChild(document.createTextNode(cellValue));
         td.setAttribute('field', field);
         td.style.textAlign = headerRows['visibleTitle'][visibleRow]['textAlign'];
@@ -206,6 +191,23 @@ function handlePPB(tr, row){
         td.style.display = 'none';       
         td.setAttribute('field', headerRows['invisible'][invisibleRow]);
     }       
+}
+
+function formatCellValue(row, field){
+    var cellValue="";
+    switch(field){
+        case 'InvoiceType':
+            if (row[field] < 1){
+                cellValue = Invoice[row[field]]
+            } else {
+                cellValue = 'Pauschale #'+(row[field])
+            }
+            break;
+        default:
+            cellValue = row[field]
+            break;
+    }
+    return cellValue
 }
 
 function addTreeBranch(row, td, field){
@@ -239,7 +241,6 @@ function addTreeContorl(row, td, field){
 }
 
 $(document).on('click', '.table-tree-control', function() {   
-    console.log("wurde getriggert")  
     var status = $(this).attr("status");
     var contolElements = $(this).attr("controlelements");
     var child = $(this).children('div')
@@ -292,3 +293,151 @@ $("#pdfButton").click(function(){
         doc.save("sample.pdf");
     });
 });
+    
+
+
+function pdf_formatTableDate(){
+    var pdfTable = [];
+    for (let key in tableDataNested){
+        var row = tableDataNested[key];
+        pdfTable.push(pdf_formatRow(row))
+        pdfTable = pdf_addChildren(pdfTable, row)
+    }    
+    return pdfTable
+}
+
+function pdf_addChildren(pdfTable, row){
+    var tmpPDFTable = pdfTable;
+    if ('_children' in row){
+        console.log("ist drin!")
+        for (child in row['_children']){
+            console.log(row['_children'])
+            var rowChild = row['_children'][child];
+            console.log(rowChild)
+            tmpPDFTable.push(pdf_formatRow(rowChild))
+            tmpPDFTable = pdf_addChildren(tmpPDFTable, rowChild)
+        }
+    } else {
+        console.log("hat keine kinder!")
+    }
+    return tmpPDFTable
+}
+
+function pdf_formatRow(row){
+    var formattedRow = {}
+    for (let cell in headerRows['visible']){
+        var field = headerRows['visible'][cell]
+        if (field in row){
+            console.log("schreibt in feld "+field)
+            formattedRow[field] = formatCellValue(row, field);
+        }
+    }
+    return formattedRow
+}
+
+function demoFromHTML(){
+    window.jsPDF = window.jspdf.jsPDF;
+    var doc = new jsPDF('p', 'pt', 'letter');  
+    var pdfTable = pdf_formatTableDate();
+
+    console.log(pdfTable)
+
+
+
+
+    var test = 1;
+    if (test == 1){
+        doc.autoTable({
+            styles: { fillColor: [255, 0, 0] },
+            columnStyles: { 0: { halign: 'center', fillColor: [0, 255, 0] } }, // Cells in first column centered and green
+            margin: { top: 10 },
+            body: pdfTable, /*[
+                
+            ['Sweden', 'Japan', 'Canada'],
+            ['Norway', 'China', 'USA'],
+            ['Denmark', 'China', 'Mexico'],
+            
+            ],*/
+        })
+    } else {      
+      // Example usage of columns property. Note that America will not be included even though it exist in the body since there is no column specified for it.
+      doc.autoTable({
+        columnStyles: { europe: { halign: 'center' } }, // European countries centered
+        body: [
+          { europe: 'Sweden', america: 'Canada', asia: 'China' },
+          { europe: 'Norway', america: 'Mexico', asia: 'Japan' },
+        ],
+        columns: [
+          { header: 'Europe', dataKey: 'europe' },
+          { header: 'Asia', dataKey: 'asia' },
+        ],
+      })
+    }
+    
+    doc.save('ePPB.pdf');  
+}
+
+function demoFromHTML1() {
+    window.jsPDF = window.jspdf.jsPDF;
+    var doc = new jsPDF('p', 'pt', 'letter');  
+    var htmlstring = '';  
+    var tempVarToCheckPageHeight = 0;  
+    var pageHeight = 0;  
+    pageHeight = doc.internal.pageSize.height;  
+    specialElementHandlers = {  
+        // element with id of "bypass" - jQuery style selector  
+        '#bypassme': function(element, renderer) {  
+            // true = "handled elsewhere, bypass text extraction"  
+            return true  
+        }  
+    };  
+    margins = {  
+        top: 150,  
+        bottom: 60,  
+        left: 40,  
+        right: 40,  
+        width: 600  
+    };  
+    var y = 20;  
+    doc.setLineWidth(2);  
+    doc.text(200, y = y + 30, "elektronische Prüfplatzbelegung");  
+    mywidth=60;
+    doc.autoTable({  
+        html: '#main-table',  
+        startY: 70,  
+        theme: 'grid',  
+        columnStyles: {  
+            0: {  
+                cellWidth: 40,  
+            },  
+            1: {  
+                cellWidth: mywidth,  
+            },  
+            2: {  
+                cellWidth: mywidth,  
+            },    
+            3: {  
+                cellWidth: mywidth,  
+            } ,    
+            4: {  
+                cellWidth: mywidth,  
+            } ,    
+            5: {  
+                cellWidth: mywidth,  
+            } ,    
+            6: {  
+                cellWidth: mywidth,  
+            } ,    
+            7: {  
+                cellWidth: mywidth,  
+            } ,    
+            8: {  
+                cellWidth: mywidth,  
+            } 
+        },  
+        styles: {  
+            minCellHeight: 40  
+        }  
+    })  
+    doc.save('ePPB.pdf');  
+}
